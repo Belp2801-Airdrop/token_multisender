cfb61f84f1107a25733fc7b48a97495bc6e61c11
NXH2801 <ngoxuanhinham123@gmail.com>
2024-11-11 23:07:14 +0700

    feat(token_multisender): ui

---
+++ token_multisender.py
@@ -0,0 +1,298 @@
+"""
+Name: Token multisender
+Description: Token multisender 
+    - 3 mode: 
+        1. 
+        2. 
+        3. 
+Author: Belp2801
+Created: 09.11.2024
+"""
+
+import customtkinter
+import os, time, datetime
+import csv
+
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("blue")
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+class TabView(customtkinter.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.tabs = ["Type 1: 1 to n", "Type 2: n to 1", "Type 3: n to n"]
+        # create 
+        for tab in self.tabs:
+            self.add(tab)
+
+        self.init_data()
+        self.init_vars()
+        self.init_ctk_vars()
+        self.build_widgets()
+
+    def init_data(self):
+        pass
+
+    def init_vars(self):
+        self.address_vars = []
+        self.private_key_vars = []
+        self.columns_vars = []
+        self.file_vars = []
+
+    def init_ctk_vars(self):
+        for i in range(len(self.tabs)):
+            self.address_vars.append(customtkinter.StringVar())
+            self.private_key_vars.append(customtkinter.StringVar())
+            self.columns_vars.append(customtkinter.StringVar())
+            self.file_vars.append(customtkinter.StringVar())
+
+    # region widgets
+    def build_widgets(self):
+        for i, tab in enumerate(self.tabs):
+            self.build_tab_view(i, self.tab(tab))
+
+    def build_tab_view(self, i, tab):
+        self.build_address_widgets(i, tab)
+
+    def build_address_widgets(self, i, tab):
+        # Address and Private
+        if i == 0:
+            self.address_label = customtkinter.CTkLabel(tab, width=108, anchor="w", text="From Address (*):")
+            self.address_entry = customtkinter.CTkEntry(tab, textvariable=self.address_vars[i], width=360, border_width=0)
+            self.address_label.grid(row=2, column=0, padx=5, pady=5, sticky="e")
+            self.address_entry.grid(row=2, column=1, padx=5, pady=5, sticky="e")
+
+            self.private_key_label = customtkinter.CTkLabel(tab, width=108, anchor="w", text="Private Key (*):")
+            self.private_key_entry = customtkinter.CTkEntry(tab, textvariable=self.private_key_vars[i], width=360, border_width=0)
+            self.private_key_label.grid(row=3, column=0, padx=5, pady=5, sticky="e")
+            self.private_key_entry.grid(row=3, column=1, padx=5, pady=5, sticky="e")
+
+        elif i == 1:
+            self.address_label = customtkinter.CTkLabel(tab, width=108, anchor="w", text="To Address (*):")
+            self.address_entry = customtkinter.CTkEntry(tab, textvariable=self.address_vars[i], width=360, border_width=0)
+            self.address_label.grid(row=2, column=0, padx=5, pady=5, sticky="e")
+            self.address_entry.grid(row=2, column=1, padx=5, pady=5, sticky="e")
+
+        csv_columns_label = customtkinter.CTkLabel(tab, width=108, anchor="w", text="Columns: ")
+        csv_columns_entry = customtkinter.CTkEntry(
+            tab, width=360, border_width=0, state="disabled", textvariable=self.columns_vars[i]
+        )
+        csv_sample_button = customtkinter.CTkButton(
+            tab, command=self.get_sample_file, width=80, text="Sample", hover=True
+        )
+        csv_columns_label.grid(row=0, column=0, padx=5, pady=4, sticky="e")
+        csv_columns_entry.grid(row=0, column=1, padx=5, pady=5)
+        csv_sample_button.grid(row=0, column=2, padx=5, pady=4)
+        
+        csv_upload_label = customtkinter.CTkLabel(tab, width=108, anchor="w", text="File (*): ")
+        csv_upload_entry = customtkinter.CTkEntry(tab, width=360, border_width=0)
+        csv_upload_button = customtkinter.CTkButton(
+            tab, width=80, text="Select"
+        )
+        csv_upload_label.grid(row=1, column=0, padx=5, pady=4, sticky="e")
+        csv_upload_entry.grid(row=1, column=1, padx=5, pady=4)
+        csv_upload_button.grid(row=1, column=2, padx=5, pady=4)
+    # endregion
+
+    # region utils
+    def get_current_tab_index(self):
+        return self.index(self.get())
+    
+    def handle_get_csv_columns(self, tab, mode):        
+        if tab == 0:
+            if mode == 1 or mode == 2:
+                return ["to_address"]
+            elif mode == 3:
+                return ["to_address", "amount"]
+        elif tab == 1:
+            if mode == 1 or mode == 2:
+                return ["from_address", "private_key"]
+            elif mode == 3:
+                return ["from_address", "private_key", "amount"]
+        elif tab == 2:
+            if mode == 1 or mode == 2:
+                return ["from_address", "private_key", "to_address"]
+            elif mode == 3:
+                return ["from_address", "private_key", "to_address", "amount"]
+                    
+                
+    def handle_set_columns_vars(self, mode):
+        for i in range(len(self.tabs)):
+            columns = self.handle_get_csv_columns(i, mode)
+            self.columns_vars[i].set(f"[{', '.join(columns)}]")
+        
+    def get_sample_file(self):
+        pass
+    # endregion
+
+
+class TokenMultiSender(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Token MultiSender")
+        self.resizable(False, False)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+
+        self.init_constants()
+        self.init_data()
+        self.init_ctk_vars()
+
+        self.build_widgets()
+
+        self.tab_view.handle_set_columns_vars(self.mode_var.get())
+    # region 
+    def init_constants(self):
+        pass
+
+    def init_data(self):
+        # networks
+        self.networks = {}
+        self.load_network_data()
+
+        # token types
+        self.token_types = ["TOKEN", "NFT"]
+        # modes
+        self.modes = {1: "All", 2: "Amount", 3: "Custom in file"} 
+
+    def load_network_data(self):
+        with open("./data/networks.csv", "r") as f:
+            reader = csv.DictReader(f)
+            for line in reader:
+                self.networks[line["name"]] = line
+
+    def init_ctk_vars(self):
+        self.network_var = customtkinter.StringVar()
+        self.token_type_var = customtkinter.StringVar()
+
+        self.mode_var = customtkinter.IntVar()
+        self.mode_var.set(1)
+        self.value_var = customtkinter.DoubleVar()
+
+        self.token_address_var = customtkinter.StringVar()
+        self.columns_var = customtkinter.StringVar()
+
+    # endregion
+
+    # region build ui
+    def build_widgets(self):
+        self.build_type_frame()
+        self.build_token_data_frame()
+        self.build_tab_views()
+        self.build_footers()
+
+    def build_type_frame(self):
+        self.type_frame = customtkinter.CTkFrame(self, corner_radius=5)
+        self.type_frame.pack(padx=5, pady=(5, 0), fill="both", expand=True)
+
+        self.build_network_widgets()
+        self.build_token_type_widgets()
+
+        self.network_frame.pack(side=customtkinter.LEFT)
+        self.token_type_frame.pack(side=customtkinter.RIGHT)
+
+    def build_network_widgets(self):
+        # Network
+        self.network_frame = customtkinter.CTkFrame(self.type_frame, corner_radius=5, bg_color="transparent", fg_color="transparent")
+        self.network_label = customtkinter.CTkLabel(self.network_frame, width=108, anchor="w", text="Network:")
+        self.network_combobox = customtkinter.CTkOptionMenu(
+            self.network_frame, 
+            variable=self.network_var, 
+            values=sorted(self.networks.keys()),
+            fg_color=["#F9F9FA", "#343638"],
+            text_color=["#000", "#fff"],
+        )
+        self.network_combobox.set(sorted(self.networks.keys())[0])
+
+        self.network_label.grid(row=0, column=0, padx=(16, 5), pady=8, sticky="w")
+        self.network_combobox.grid(row=0, column=1, padx=5, pady=8, sticky="w")
+
+    def build_token_type_widgets(self):
+        # Token type
+        self.token_type_frame = customtkinter.CTkFrame(self.type_frame, corner_radius=5, bg_color="transparent", fg_color="transparent")
+        self.token_type_label = customtkinter.CTkLabel(self.token_type_frame, text="Token Type:")
+        self.token_type_combobox = customtkinter.CTkOptionMenu(
+            self.token_type_frame, 
+            variable=self.token_type_var, 
+            values=self.token_types,
+            fg_color=["#F9F9FA", "#343638"],
+            text_color=["#000", "#fff"]
+        )
+        self.token_type_combobox.set(self.token_types[0])
+
+        self.token_type_label.grid(row=0, column=2, padx=(16, 5), pady=8, sticky="e")
+        self.token_type_combobox.grid(row=0, column=3, padx=(16, 18), pady=8, sticky="e")
+
+    def build_token_data_frame(self):
+        self.token_data_frame = customtkinter.CTkFrame(self)
+        self.token_data_frame.pack(padx=5, pady=(5, 0), fill="both", expand=True)
+
+        self.build_token_address_widgets()
+        self.build_mode_widgets()
+
+    def build_mode_widgets(self):
+        # Mode
+        def on_select_mode(choice):
+            for key, value in self.modes.items():
+                if value == choice:
+                    self.mode_var.set(key)
+
+            self.tab_view.handle_set_columns_vars(self.mode_var.get())
+            if self.mode_var.get() == 2:
+                self.build_value_widgets()
+            else:
+                self.value_label.grid_forget()
+                self.value_entry.grid_forget()
+            
+        self.mode_label = customtkinter.CTkLabel(self.token_data_frame, width=108, anchor="w", text="Mode (*):")
+        self.mode_combobox = customtkinter.CTkOptionMenu(
+            self.token_data_frame, 
+            values=list(self.modes.values()),
+            command=on_select_mode,
+            fg_color=["#F9F9FA", "#343638"],
+            text_color=["#000", "#fff"]
+        )
+        self.mode_combobox.set(list(self.modes.values())[0])
+        self.mode_label.grid(row=2, column=0, padx=(16, 5), pady=5, sticky="w")
+        self.mode_combobox.grid(row=2, column=1, padx=5, pady=5, sticky="w")
+
+    def build_value_widgets(self):
+        self.value_label = customtkinter.CTkLabel(self.token_data_frame, width=96, anchor="e", text="Value:")
+        self.value_entry = customtkinter.CTkEntry(self.token_data_frame, width=65, justify=customtkinter.RIGHT, textvariable=self.value_var, border_width=0)
+
+        self.value_label.grid(row=2, column=2, padx=(32, 5), pady=5, sticky="e")
+        self.value_entry.grid(row=2, column=3, padx=(5, 0), pady=5, sticky="w")
+
+    def build_token_address_widgets(self):
+        # Token address
+        self.token_address_label = customtkinter.CTkLabel(self.token_data_frame, width=108, anchor="w",  text="Token address:")
+        self.token_address_entry = customtkinter.CTkEntry(self.token_data_frame, textvariable=self.token_address_var, width=360, border_width=0)
+        self.token_address_button = customtkinter.CTkButton(self.token_data_frame, text="Check", width=80)
+        self.token_address_label.grid(row=1, column=0, padx=(16, 5), pady=(8, 5), sticky="w")
+        self.token_address_entry.grid(row=1, column=1, columnspan=3, padx=5, pady=(8, 5), sticky="w")
+        self.token_address_button.grid(row=1, column=4, padx=5, pady=(8, 5), sticky="w")
+        
+    def build_tab_views(self):
+        self.tab_view = TabView(master=self)
+        self.tab_view.pack(padx=5, pady=(0, 0), fill="both", expand=True)
+
+    def build_footers(self):
+        self.footers = customtkinter.CTkLabel(self, text="@Powered by: Belp2801")
+        self.footers.pack(padx=5, pady=4)
+    # endregion
+    
+    # region utils
+    def get_current_tab_index(self):
+        return self.tab_view.index(self.tab_view.get())
+    # endregion
+
+    # region run
+    # endregion
+
+
+if __name__ == "__main__":
+    app = TokenMultiSender()
+    app.mainloop()
+    
